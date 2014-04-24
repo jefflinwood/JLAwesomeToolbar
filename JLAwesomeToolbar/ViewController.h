c7b7efad2ea81a108ed9d09b010c3745922c61c8
Jeffrey Linwood <jlinwood@gmail.com>
2014-04-24 14:34:19 -0500

    Initial Commit

---
+++ JLAwesomeToolbar/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JLAwesomeToolbar
+//
+//  Created by Jeffrey Linwood on 4/24/14.
+//  Copyright (c) 2014 Jeff Linwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ JLAwesomeToolbar/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JLAwesomeToolbar
+//
+//  Created by Jeffrey Linwood on 4/24/14.
+//  Copyright (c) 2014 Jeff Linwood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
